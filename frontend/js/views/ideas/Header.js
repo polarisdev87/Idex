34151c9d4e4e513cc02bf26932948ab67fca4ba0
polarisdev87 <polarisdev87@gmail.com>
2017-11-12 01:01:38 +0700

    Select filter top/newest

--- frontend/js/views/ideas/Header.js
+++ frontend/js/views/ideas/Header.js
@@ -11,10 +11,26 @@ type Props = {
 
 class Header extends Component {
   props: Props;
-    
+
+  componentDidMount() {
+    this.state = {
+      filterText: "Top - Past Day (Default)",
+      anyStageTicked: true
+    }
+  }
+
   showContent() {
     const $this = $(findDOMNode(this));
     $this.find('#collapse-container-body').collapse('show');
+  }
+
+  test() {
+    console.log('test');
+  }
+
+  setFilterText(filterText) {
+
+    this.setState({ filterText: `Top - ${filterText}`});
   }
 
   render() {
@@ -25,10 +41,7 @@ class Header extends Component {
         <div className="nav-container">
           <div className="row">
             <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6">
-              <button type="button" className="btn btn-link" >All Ideas</button>
-              <button type="button" className="btn btn-link">Newest</button>
-              <button type="button" className="btn btn-link">Votes</button>
-              <button type="button" className="btn btn-link" data-toggle="collapse" href="#collapse-container-body">Tags</button>
+              <button type="button" className="btn btn-link" data-toggle="collapse" href="#collapse-container-body">Filter</button>
             </div>
             <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6">
               <button type="button" className="btn btn-link btn-right" onClick={() => addIdeaButtonClick()}>Add Idea</button>
@@ -90,20 +103,20 @@ class Header extends Component {
                 <div className="dropdown-container">
                   <ul className="nav site-nav">
                     <li className="flyout">
-                      <a href="#" className="dropdown-title">▼</a>
+                      <a href="#" className="dropdown-title"><span id="selected">{ this.state.filterText }</span><span>▼</span></a>
                       <ul className="flyout-content sub nav stacked">
-                        <li><a href="#">Votes (default)</a></li>
                         <li className="flyout-alt">
-                          <a href="#">Newest</a>
+                          <a href="#">Top (default)</a>
                           <ul className="flyout-content sub nav stacked">
-                            <li><a href="#">Past Hour</a></li>
-                            <li><a href="#">Past day (default)</a></li>
-                            <li><a href="#">Past week</a></li>
-                            <li><a href="#">Past month</a></li>
-                            <li><a href="#">Past year</a></li>
-                            <li><a href="#">All time</a></li>
+                            <li><a href="#" onClick={ () => this.setFilterText('Past Hour') }>Past Hour</a></li>
+                            <li><a href="#" onClick={ () => this.setFilterText('Past Day') }>Past Day (default)</a></li>
+                            <li><a href="#" onClick={ () => this.setFilterText('Past Week') }>Past Week</a></li>
+                            <li><a href="#" onClick={ () => this.setFilterText('Past Month') }>Past Month</a></li>
+                            <li><a href="#" onClick={ () => this.setFilterText('Past Year') }>Past Year</a></li>
+                            <li><a href="#" onClick={ () => this.setFilterText('All Time') }>All Time</a></li>
                           </ul>
                         </li>
+                        <li><a href="#">Newest</a></li>
                       </ul>
                     </li>
                   </ul>​
@@ -114,7 +127,8 @@ class Header extends Component {
               <div className="label-base-base">
                 72 Results
               </div>
-              <button type="button" className="btn btn-link label-base-base" >Clear Filters</button>
+              <button type="button" className="btn btn-link label-base-base" onClick={ this.test } >Apply Filters</button>
+              <button type="button" className="btn btn-link label-base-base" onClick={ this.test } >Clear Filters</button>
             </div>
           </div>
         </div>
