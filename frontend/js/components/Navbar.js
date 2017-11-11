9c917be63b4036ca01deae88eb21a1b83b3d0a40
polarisdev87 <polarisdev87@gmail.com>
2017-11-12 01:40:34 +0700

    Remove Users from navbar. Only show logout if logged in

--- frontend/js/components/Navbar.js
+++ frontend/js/components/Navbar.js
@@ -22,31 +22,10 @@ export default class Navbar extends Component {
             <img src="../../assets/images/logo.png" />
           </a>
           <div className="navbar-form text-right">
-            <button type="button" className="btn btn-link label-base-base">Users</button>
-            <button type="button" className="btn btn-link label-base-base" onClick={() => dispatch(logoutUser())} >Logout</button>
-            {/* {
-              !isAuthenticated &&
-                <LoginNav
-                  errorMessage={loginErrorMessage}
-                  onLoginClick={ creds => dispatch(loginUser(creds)) }
-                  onLoginError={ message => dispatch(handleLoginError(message)) }
-                  dispatch={ dispatch }
-                />
-            }
-
-            { (role === 'ROLE_ADMIN' || role === 'ROLE_USER_MANAGER') &&
-              <span>
-                <Users />
-              </span>
-            }
-
             {
               isAuthenticated &&
-              <span>
-                <Ideas />
-                <Logout onLogoutClick={() => dispatch(logoutUser())} />
-              </span>
-            } */}
+                <button type="button" className="btn btn-link label-base-base" onClick={() => dispatch(logoutUser())} >Logout</button>
+            }
 
           </div>
         </div>
