b915030412723371950673aab9fbf451ce54c0af
polarisdev87 <polarisdev87@gmail.com>
2019-03-17 10:18:11 -0300

    full / partial match individual tag approach

--- frontend/js/components/tags/TagSection.js
+++ frontend/js/components/tags/TagSection.js
@@ -15,6 +15,7 @@ class TagSection extends Component {
 
   state = {
     inputValue: '',
+    optionalMarks: [],
   }
 
   handleChange(tags) {
@@ -29,6 +30,32 @@ class TagSection extends Component {
     }
   }
 
+
+  renderOptionalTag(props) {
+    const {
+      tag, key, disabled, onRemove, classNameRemove, getTagDisplayValue, ...other
+    } = props;
+    console.log("renderOptionalTag");
+    console.log(key);
+
+
+
+    return (
+      <span key={key} {...other}>
+        {getTagDisplayValue(tag)}
+        {!disabled &&
+        <a className={classNameRemove} onClick={(e) => onRemove(key)} />
+        }
+        <div className="checkbox label-xs-base">
+          <label>
+            <input type="checkbox" value="" checked={true} />
+            <span className="cr"><i className="cr-icon glyphicon glyphicon-ok" /></span>
+          </label>
+        </div>
+      </span>
+    );
+  }
+
   render() {
     const { placeholder, type, className } = this.props;
     const { inputValue } = this.state;
@@ -36,7 +63,7 @@ class TagSection extends Component {
       <div className="form-group tag-container">
         <div className="select-label label-base-base">Select Tags:</div>
         <div className="input-container display-tag-container">
-          <TagsInput value={this.props.tags} onChange={::this.handleChange} />
+          <TagsInput value={this.props.tags} onChange={::this.handleChange} renderTag={this.renderOptionalTag} />
         </div>
         <div className="top-tag-container">
           <div className="label-sm-base trending-label">Top Trending Tags:</div>
@@ -55,7 +82,10 @@ class TagSection extends Component {
         <div className="input-container search-input-container">
           <div className="row">
             <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6">
-              <input type="text" className="form-control search-input" placeholder="Search tags ....." ref={el => { this.searchTag = el; }} onKeyPress={this.handleKeyPress} />
+              <input
+                type="text" className="form-control search-input" placeholder="Search tags ....." ref={el => { this.searchTag = el; }}
+                onKeyPress={this.handleKeyPress}
+              />
             </div>
           </div>
         </div>
--- frontend/js/views/ideas/Header.js
+++ frontend/js/views/ideas/Header.js
@@ -19,12 +19,12 @@ class Header extends Component {
   props: Props;
 
   topTypeFilters = {
-    pastHour : "Past Hour",
-    pastDay : 'Past Day',
-    pastWeek : 'Past Week',
-    pastMonth : 'Past Month',
-    pastYear : 'Past Year',
-    allTime : 'All Time'
+    pastHour: 'Past Hour',
+    pastDay: 'Past Day',
+    pastWeek: 'Past Week',
+    pastMonth: 'Past Month',
+    pastYear: 'Past Year',
+    allTime: 'All Time',
   };
 
   constructor(props) {
@@ -48,15 +48,16 @@ class Header extends Component {
         incubation: false,
         prototyping: false,
         launched: false,
-        cancelled: false
+        cancelled: false,
       },
       tags: [],
+      optionalMarks: [],
       votesMin: 0,
       votesMax: 999999,
       profitMin: 0,
       profitMax: 999999,
-      implementationTimeMin:0,
-      implementationTimeMax:999999,
+      implementationTimeMin: 0,
+      implementationTimeMax: 999999,
     };
   }
 
@@ -68,19 +69,19 @@ class Header extends Component {
 
 
   getMomentFromLabel(timeLabel) {
-      var atMsMin=0;
-      if(timeLabel.includes('Hour')) {
-        atMsMin = moment.utc().add(-1, 'hours').valueOf();
-      } else if (timeLabel.includes('Day')) {
-        atMsMin = moment.utc().add(-1, 'days').valueOf();
-      } else if (timeLabel.includes('Week')) {
-        atMsMin = moment.utc().add(-1, 'weeks').valueOf();
-      } else if (timeLabel.includes('Month')) {
-        atMsMin = moment.utc().add(-1, 'months').valueOf();
-      } else if (timeLabel.includes('Year')) {
-        atMsMin = moment.utc().add(-1, 'years').valueOf();
-      }
-      return atMsMin;
+    let atMsMin = 0;
+    if (timeLabel.includes('Hour')) {
+      atMsMin = moment.utc().add(-1, 'hours').valueOf();
+    } else if (timeLabel.includes('Day')) {
+      atMsMin = moment.utc().add(-1, 'days').valueOf();
+    } else if (timeLabel.includes('Week')) {
+      atMsMin = moment.utc().add(-1, 'weeks').valueOf();
+    } else if (timeLabel.includes('Month')) {
+      atMsMin = moment.utc().add(-1, 'months').valueOf();
+    } else if (timeLabel.includes('Year')) {
+      atMsMin = moment.utc().add(-1, 'years').valueOf();
+    }
+    return atMsMin;
   }
 
 
@@ -92,85 +93,88 @@ class Header extends Component {
 
 
   addTag(value) {
-      const newTags = this.state.tags;
-      newTags.push(value);
-      this.setState({
-        tags: newTags,
-      });
+    const newTags = this.state.tags;
+    const newOptionalMarks = this.state.optionalMarks;
+
+    newTags.push(value);
+    this.setState({
+      tags: newTags,
+      optionalMarks: newOptionalMarks,
+    });
   }
 
 
   changeVotesMin(value) {
-    let votesMax=value > this.state.votesMax ? value: this.state.votesMax;
-    this.setState({ votesMin: value, votesMax: votesMax });
+    const votesMax = value > this.state.votesMax ? value : this.state.votesMax;
+    this.setState({ votesMin: value, votesMax });
   }
 
   changeVotesMax(value) {
-    let votesMin=value < this.state.votesMin ? value: this.state.votesMin;
-    this.setState({ votesMax: value, votesMin:votesMin });
+    const votesMin = value < this.state.votesMin ? value : this.state.votesMin;
+    this.setState({ votesMax: value, votesMin });
   }
-  
+
 
   changeProfitMin(value) {
-    let profitMax=value > this.state.profitMax ? value: this.state.profitMax;
-    this.setState({ profitMin: value, profitMax: profitMax });
+    const profitMax = value > this.state.profitMax ? value : this.state.profitMax;
+    this.setState({ profitMin: value, profitMax });
   }
 
   changeProfitMax(value) {
-    let profitMin=value < this.state.profitMin ? value: this.state.profitMin;
-    this.setState({ profitMax: value, profitMin: profitMin });
+    const profitMin = value < this.state.profitMin ? value : this.state.profitMin;
+    this.setState({ profitMax: value, profitMin });
   }
 
 
   changeImplementationTimeMin(value) {
-    let implementationTimeMax=value > this.state.implementationTimeMax ? value: this.state.implementationTimeMax;
-    this.setState({ implementationTimeMin: value, implementationTimeMax: implementationTimeMax });
-  } 
-  
+    const implementationTimeMax = value > this.state.implementationTimeMax ? value : this.state.implementationTimeMax;
+    this.setState({ implementationTimeMin: value, implementationTimeMax });
+  }
+
   changeImplementationTimeMax(value) {
-    let implementationTimeMin=value < this.state.implementationTimeMin ? value: this.state.implementationTimeMin;
-    this.setState({ implementationTimeMax: value, implementationTimeMin:implementationTimeMin });
-  } 
+    const implementationTimeMin = value < this.state.implementationTimeMin ? value : this.state.implementationTimeMin;
+    this.setState({ implementationTimeMax: value, implementationTimeMin });
+  }
 
 
   applyFilters() {
-
     console.log('this', this);
-    const { filterText, 
-        stagesSelected, 
-        implementationTimeMin,
-        implementationTimeMax, 
-        votesMin, 
-        votesMax, 
-        profitMin, 
-        profitMax, 
-        tags } = this.state;
+    const {
+      filterText,
+      stagesSelected,
+      implementationTimeMin,
+      implementationTimeMax,
+      votesMin,
+      votesMax,
+      profitMin,
+      profitMax,
+      tags,
+      optionalMarks, 
+} = this.state;
 
-    let stages = [];
+    const stages = [];
 
-    if(stagesSelected['any'] === true) {
+    if (stagesSelected.any === true) {
       stages.push('Incubation');
       stages.push('Prototyping');
       stages.push('Launched');
       stages.push('Cancelled');
     } else {
-      stagesSelected['incubation'] === true ? stages.push('Incubation') : null;
-      stagesSelected['prototyping'] === true ? stages.push('Prototyping') : null;
-      stagesSelected['launched'] === true ? stages.push('Launched') : null;
-      stagesSelected['cancelled'] === true ? stages.push('Cancelled') : null;
-
+      stagesSelected.incubation === true ? stages.push('Incubation') : null;
+      stagesSelected.prototyping === true ? stages.push('Prototyping') : null;
+      stagesSelected.launched === true ? stages.push('Launched') : null;
+      stagesSelected.cancelled === true ? stages.push('Cancelled') : null;
     }
 
-    let mainFilter="";
+    let mainFilter = '';
     let submittedAtMsMin = null;
-    let submittedAtMsMax = null;
-    if(filterText.startsWith('Top')) {
-        submittedAtMsMin = this.getMomentFromLabel(filterText);
+    const submittedAtMsMax = null;
+    if (filterText.startsWith('Top')) {
+      submittedAtMsMin = this.getMomentFromLabel(filterText);
     } else {
-        mainFilter = 'Newest';
+      mainFilter = 'Newest';
     }
-    this.props.fetchIdeas(mainFilter, stages, submittedAtMsMin,submittedAtMsMax,votesMin,votesMax,profitMin,profitMax,implementationTimeMin,implementationTimeMax, tags);
-
+    this.props.fetchIdeas(mainFilter, stages, submittedAtMsMin, submittedAtMsMax, votesMin, votesMax, profitMin, profitMax, implementationTimeMin, implementationTimeMax, tags);
   }
 
   clearFilters() {
@@ -178,38 +182,36 @@ class Header extends Component {
   }
 
   setFilterText(filterText) {
-
-    if(filterText !== 'Newest') {
+    if (filterText !== 'Newest') {
       this.setState({
-          filterText: `Top - ${filterText}`,
-          stagesSelected: this.state.stagesSelected
-        });
+        filterText: `Top - ${filterText}`,
+        stagesSelected: this.state.stagesSelected,
+      });
     } else {
-      this.setState({ filterText: filterText });
+      this.setState({ filterText });
     }
   }
 
 
   setFilterImplementedText(filterText) {
-      this.setState({
-          filterImplementedText: `${filterText}`,
-        });
+    this.setState({
+      filterImplementedText: `${filterText}`,
+    });
   }
 
 
   checkStageBox(stageBox) {
+    const stagesSelected = this.state.stagesSelected;
+    stagesSelected[stageBox] = !stagesSelected[stageBox];
 
-    let stagesSelected = this.state.stagesSelected;
-    stagesSelected[stageBox] =  !stagesSelected[stageBox];
-
-    if(stageBox !== 'any' && stagesSelected['any'] === true) {
-      stagesSelected['any'] = false;
+    if (stageBox !== 'any' && stagesSelected.any === true) {
+      stagesSelected.any = false;
     }
 
-    this.setState( {
+    this.setState({
       stagesSelected,
-      filterText: this.state.filterText
-    })
+      filterText: this.state.filterText,
+    });
   }
 
 
@@ -221,10 +223,10 @@ class Header extends Component {
     return (
       <div className="header-container shadow">
 
-            {/* Header */}
-      
-      	<div className="nav-container">
-        <div className="row">
+        {/* Header */}
+
+        <div className="nav-container">
+          <div className="row">
             <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6">
               <button type="button" className="btn btn-link" data-toggle="collapse" href="#collapse-container-body">Filter</button>
             </div>
@@ -234,16 +236,18 @@ class Header extends Component {
           </div>
         </div>
 
-        
-        
         <div id="collapse-container-body" className="collapse">
           <div className="collapse-container">
             <div className="tag-section">
-              <TagSection tags={this.state.tags} handleTagsChange={(tags) => this.handleTagsChange(tags)} addTag={(tag) => this.addTag(tag)} />
+              <TagSection
+                tags={this.state.tags}
+                handleTagsChange={(tags) => this.handleTagsChange(tags)}
+                addTag={(tag) => this.addTag(tag)}
+              />
             </div>
-        
+
             {/* Stage */}
-            
+
             <div className="row">
               <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6">
                 <div className="label-base-base">
@@ -252,35 +256,35 @@ class Header extends Component {
                 <div className="">
                   <div className="checkbox label-xs-base">
                     <label>
-                      <input type="checkbox" value="" checked={stagesSelected.any} onChange={ () =>  this.checkStageBox('any') } />
+                      <input type="checkbox" value="" checked={stagesSelected.any} onChange={() => this.checkStageBox('any')} />
                       <span className="cr"><i className="cr-icon glyphicon glyphicon-ok" /></span>
                       Any Stage (default)
                     </label>
                   </div>
                   <div className="checkbox label-xs-base">
                     <label>
-                      <input type="checkbox" value="" checked={ stagesSelected.any || stagesSelected.incubation } onChange={ () =>  this.checkStageBox('incubation') } />
+                      <input type="checkbox" value="" checked={stagesSelected.any || stagesSelected.incubation} onChange={() => this.checkStageBox('incubation')} />
                       <span className="cr"><i className="cr-icon glyphicon glyphicon-ok" /></span>
                       Incubation
                     </label>
                   </div>
                   <div className="checkbox label-xs-base">
                     <label>
-                      <input type="checkbox" value="" checked={ stagesSelected.any || stagesSelected.prototyping } onChange={ () =>  this.checkStageBox('prototyping') } />
+                      <input type="checkbox" value="" checked={stagesSelected.any || stagesSelected.prototyping} onChange={() => this.checkStageBox('prototyping')} />
                       <span className="cr"><i className="cr-icon glyphicon glyphicon-ok" /></span>
                       Prototyping
                     </label>
                   </div>
                   <div className="checkbox label-xs-base">
                     <label>
-                      <input type="checkbox" value=""  checked={ stagesSelected.any || stagesSelected.launched } onChange={ () =>  this.checkStageBox('launched') } />
+                      <input type="checkbox" value="" checked={stagesSelected.any || stagesSelected.launched} onChange={() => this.checkStageBox('launched')} />
                       <span className="cr"><i className="cr-icon glyphicon glyphicon-ok" /></span>
                       Launched
                     </label>
                   </div>
                   <div className="checkbox label-xs-base">
                     <label>
-                      <input type="checkbox" value="" checked={stagesSelected.any || stagesSelected.cancelled } onChange={ () =>  this.checkStageBox('cancelled') } />
+                      <input type="checkbox" value="" checked={stagesSelected.any || stagesSelected.cancelled} onChange={() => this.checkStageBox('cancelled')} />
                       <span className="cr"><i className="cr-icon glyphicon glyphicon-ok" /></span>
                       Cancelled
                     </label>
@@ -288,7 +292,7 @@ class Header extends Component {
                 </div>
               </div>
 
-            {/* Sort by section */}
+              {/* Sort by section */}
               <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6">
                 <div className="label-base-base">
                   Sort by:
@@ -301,61 +305,63 @@ class Header extends Component {
                         <li className="flyout-alt">
                           <a href="#">Top (default)</a>
                           <ul className="flyout-content sub nav stacked">
-                            <li><a href="#" onClick={ () => this.setFilterText(this.topTypeFilters.pastHour) }>{this.topTypeFilters.pastHour}</a></li>
-                            <li><a href="#" onClick={ () => this.setFilterText(this.topTypeFilters.pastDay) }>{this.topTypeFilters.pastDay} (Default)</a></li>
-                            <li><a href="#" onClick={ () => this.setFilterText(this.topTypeFilters.pastWeek) }>{this.topTypeFilters.pastWeek}</a></li>
-                            <li><a href="#" onClick={ () => this.setFilterText(this.topTypeFilters.pastMonth) }>{this.topTypeFilters.pastMonth}</a></li>
-                            <li><a href="#" onClick={ () => this.setFilterText(this.topTypeFilters.pastYear) }>{this.topTypeFilters.pastYear}</a></li>
-                            <li><a href="#" onClick={ () => this.setFilterText(this.topTypeFilters.allTime) }>{this.topTypeFilters.allTime}</a></li>
+                            <li><a href="#" onClick={() => this.setFilterText(this.topTypeFilters.pastHour)}>{this.topTypeFilters.pastHour}</a></li>
+                            <li><a href="#" onClick={() => this.setFilterText(this.topTypeFilters.pastDay)}>{this.topTypeFilters.pastDay} (Default)</a></li>
+                            <li><a href="#" onClick={() => this.setFilterText(this.topTypeFilters.pastWeek)}>{this.topTypeFilters.pastWeek}</a></li>
+                            <li><a href="#" onClick={() => this.setFilterText(this.topTypeFilters.pastMonth)}>{this.topTypeFilters.pastMonth}</a></li>
+                            <li><a href="#" onClick={() => this.setFilterText(this.topTypeFilters.pastYear)}>{this.topTypeFilters.pastYear}</a></li>
+                            <li><a href="#" onClick={() => this.setFilterText(this.topTypeFilters.allTime)}>{this.topTypeFilters.allTime}</a></li>
                           </ul>
                         </li>
-                        <li><a href="#" onClick={ () => this.setFilterText('Newest') }>Newest</a></li>
+                        <li><a href="#" onClick={() => this.setFilterText('Newest')}>Newest</a></li>
                       </ul>
                     </li>
-                  </ul>​
+                  </ul>
                 </div>
               </div>
             </div>
 
             <div className="row">
-            {/* Expected Time To Market */}
+              {/* Expected Time To Market */}
               <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6">
-                <TimeToMarketFilterSection 
-                    min={this.state.implementationTimeMin} 
-                    max={this.state.implementationTimeMax} 
-                    changeMin={(value) => this.changeImplementationTimeMin(value)} 
-                    changeMax={(value) => this.changeImplementationTimeMax(value) } />
+                <TimeToMarketFilterSection
+                  min={this.state.implementationTimeMin}
+                  max={this.state.implementationTimeMax}
+                  changeMin={(value) => this.changeImplementationTimeMin(value)}
+                  changeMax={(value) => this.changeImplementationTimeMax(value)}
+                />
               </div>
 
 
-              
-            {/* Votes Section */}
+              {/* Votes Section */}
               <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6">
-                <VotesFilterSection 
-                    min={this.state.votesMin} 
-                    max={this.state.votesMax} 
-                    changeMin={(value) => this.changeVotesMin(value)} 
-                    changeMax={(value) => this.changeVotesMax(value) } />
+                <VotesFilterSection
+                  min={this.state.votesMin}
+                  max={this.state.votesMax}
+                  changeMin={(value) => this.changeVotesMin(value)}
+                  changeMax={(value) => this.changeVotesMax(value)}
+                />
               </div>
-              
-            {/* Profit Section */}
+
+              {/* Profit Section */}
               <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6">
-                <ProfitFilterSection 
-                    min={this.state.profitMin} 
-                    max={this.state.profitMax}  
-                    changeMin={(value) => this.changeProfitMin(value) } 
-                    changeMax={(value) => this.changeProfitMax(value)} />
+                <ProfitFilterSection
+                  min={this.state.profitMin}
+                  max={this.state.profitMax}
+                  changeMin={(value) => this.changeProfitMin(value)}
+                  changeMax={(value) => this.changeProfitMax(value)}
+                />
               </div>
             </div>
-            
+
 
             {/* Base Section */}
             <div className="result-container">
               <div className="label-base-base">
                 <span>{ this.props.numIdeas } Results</span>
               </div>
-              <button type="button" className="btn btn-link label-base-base" onClick={ this.applyFilters.bind(this) } >Apply Filters</button>
-              <button type="button" className="btn btn-link label-base-base" onClick={ this.clearFilters.bind(this) } >Clear Filters</button>
+              <button type="button" className="btn btn-link label-base-base" onClick={this.applyFilters.bind(this)} >Apply Filters</button>
+              <button type="button" className="btn btn-link label-base-base" onClick={this.clearFilters.bind(this)} >Clear Filters</button>
             </div>
           </div>
         </div>
@@ -372,8 +378,6 @@ function mapStateToProps(state) {
 function mapDispatchToProps(dispatch) {
   return { dispatch };
 }
-
-
 
 
 export default connect(mapStateToProps, mapDispatchToProps)(Header);
