cf514aefdd2e353685a3b86b64283fe36577cd1f
polarisdev87 <polarisdev87@gmail.com>
2019-03-18 17:49:52 -0300

    toggle partial full tags on filters first frontend approach

--- frontend/js/components/tags/TagSection.js
+++ frontend/js/components/tags/TagSection.js
@@ -1,8 +1,7 @@
 // @flow
-import React, { Component, PropTypes } from 'react';
-import cx from 'classnames';
-
+import React, { Component } from 'react';
 import TagsInput from 'react-tagsinput';
+import Toggle from 'react-bootstrap-toggle';
 
 type Props = {
   placeholder: '',
@@ -22,6 +21,42 @@ class TagSection extends Component {
     this.props.handleTagsChange(tags);
   }
 
+  onToggle(x) {
+    console.log(this);
+    const newToggleActive = !this.state.toggleActive;
+    this.setState({ toggleActive: newToggleActive });
+  }
+
+
+  /*
+      <span>
+
+  */
+
+  defaultRenderLayout(tagComponents, inputComponent, toggleActive) {
+    console.log('defaultRender');
+    console.log(tagComponents);
+    console.log(inputComponent);
+    return (
+      <span>
+          { tagComponents.length>0 && 
+        <span>
+          <Toggle
+            onClick={(x) => { this.onToggle(x) ;}}
+            on="Partial"
+            off="Full"
+            size="sm"
+            active={toggleActive}
+          />
+        </span>
+          }
+
+        {tagComponents}
+        {inputComponent}
+      </span>
+    );
+  }
+
   handleKeyPress = e => {
     if (e.key === 'Enter') {
       console.log(e.target.value);
@@ -31,31 +66,6 @@ class TagSection extends Component {
   }
 
 
-  renderOptionalTag(props) {
-    const {
-      tag, key, disabled, onRemove, classNameRemove, getTagDisplayValue, ...other
-    } = props;
-    console.log("renderOptionalTag");
-    console.log(key);
-
-
-
-    return (
-      <span key={key} {...other}>
-        {getTagDisplayValue(tag)}
-        {!disabled &&
-        <a className={classNameRemove} onClick={(e) => onRemove(key)} />
-        }
-        <div className="checkbox label-xs-base">
-          <label>
-            <input type="checkbox" value="" checked={true} />
-            <span className="cr"><i className="cr-icon glyphicon glyphicon-ok" /></span>
-          </label>
-        </div>
-      </span>
-    );
-  }
-
   render() {
     const { placeholder, type, className } = this.props;
     const { inputValue } = this.state;
@@ -63,7 +73,7 @@ class TagSection extends Component {
       <div className="form-group tag-container">
         <div className="select-label label-base-base">Select Tags:</div>
         <div className="input-container display-tag-container">
-          <TagsInput value={this.props.tags} onChange={::this.handleChange} renderTag={this.renderOptionalTag} />
+          <TagsInput value={this.props.tags} onChange={::this.handleChange} renderLayout ={(a, b) => this.defaultRenderLayout(a, b, this.state.toggleActive)} />
         </div>
         <div className="top-tag-container">
           <div className="label-sm-base trending-label">Top Trending Tags:</div>
@@ -78,6 +88,10 @@ class TagSection extends Component {
               web
             </div>
           </div>
+          {this.props.tags.length > 0 &&
+          <div />
+
+          }
         </div>
         <div className="input-container search-input-container">
           <div className="row">
