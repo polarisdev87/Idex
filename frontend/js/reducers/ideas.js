dc57728c3800b33a10519ec41d34b30de47963fd
polarisdev87 <polarisdev87@gmail.com>
2019-04-25 07:44:47 -0300

    clean reducer trace logs

--- frontend/js/reducers/ideas.js
+++ frontend/js/reducers/ideas.js
@@ -22,46 +22,32 @@ import { API_BASE_URI } from '../const';
 
 
 function updateFileOnElement(element, htmlFormFile, uploadedFileMeta) {
-	console.log("updateFileOnElement");
-	console.log(element);
-	
-	console.log("htmlFormFile");
-	console.log(htmlFormFile);
     if (element.files == null) {
     	element.files = [];
       }
       const fileIndex = element.files.findIndex(x => x.id === htmlFormFile.id);
       if (fileIndex !== -1) {
-      	console.log('initial file');
-      	console.log(element.files[fileIndex]);
         const newFile = Object.assign({}, element.files[fileIndex], {
           cancelledAt: uploadedFileMeta.cancelledAt,
           persistenceId: uploadedFileMeta.persistenceId,
           uploadedAt: uploadedFileMeta.uploadedAt,
         });
-    	console.log('newFile');
-    	console.log(newFile);
     	element.files = [
           ...element.files.slice(0, fileIndex),
           newFile,
           ...element.files.slice(fileIndex + 1),
         ];
       } else {
-      	console.log('htmlFormFile');
-      	console.log(htmlFormFile);
         const newFile = Object.assign({}, htmlFormFile, {
           cancelledAt: uploadedFileMeta.cancelledAt,
           persistenceId: uploadedFileMeta.persistenceId,
           uploadedAt: uploadedFileMeta.uploadedAt,
         });
-      	console.log('newFile');
-      	console.log(newFile);
       	element.files = [
           ...element.files,
           newFile,
         ];
       }
-      console.log(element);
       return element;
 }
 
@@ -94,8 +80,6 @@ function completeFileOnElement(element, htmlFormFile, uploadedFileMeta) {
                 persistenceId: uploadedFileMeta.persistenceId,
                 uploadedAt: uploadedFileMeta.uploadedAt,
               });
-              console.log("newFile");
-              console.log(newFile);
               element.files = [
                 ...element.files,
                 newFile,
@@ -108,8 +92,6 @@ function completeFileOnElement(element, htmlFormFile, uploadedFileMeta) {
 
 
 function addFileOnElement(element,file) {
-	console.log("addFileOnElement");
-	console.log(element);
     if (element.files == null) {
     	element.files = [file];
       } else {
@@ -118,7 +100,6 @@ function addFileOnElement(element,file) {
           file,
         ];
     }
-	console.log(element);
     return element;
 }
 
@@ -161,17 +142,12 @@ export function ideas(state = {
   isOpen: false,
   isOpenAttachments: false,
 }, action) {
-	 console.log("IDEAS REDUCER...");
-	 console.log(action);
-	 console.log(state);
   switch (action.type) {
     case GET_IDEAS_REQUEST:
       return Object.assign({}, state, {
         isFetchingIdeas: true,
       });
     case GET_IDEAS_SUCCESS: {
-      console.log('reducer action.ideas');
-      console.log(action.ideas);
       return Object.assign({}, state, {
         isFetchingIdeas: false,
         ideasArr: action.ideas,
@@ -215,16 +191,10 @@ export function ideas(state = {
       });
     }
     case UPDATE_IDEA_SUCCESS: {
-      console.log("UPDATE_IDEA_SUCCESS");
       let newIdeas = state.ideasArr;
       const index = state.ideasArr.findIndex(x => x.id === action.idea.id);
       if (index !== -1) {
-        console.log(state.ideasArr[index]);
-        console.log("action.idea");
-        console.log(action.idea);
         const newIdea = Object.assign({},state.ideasArr[index],action.idea);
-        console.log("newIdea");
-        console.log(newIdea);
         newIdeas = [
           ...state.ideasArr.slice(0, index),
           newIdea,
@@ -280,8 +250,6 @@ export function ideas(state = {
       });
     }
     case UPLOAD_FILE_REQUEST: {
-      console.log('UPLOAD_FILE_REQUEST');
-      console.log(action.file);
       if (action.ideaId>=0) {  // editing
           let newIdeas = state.ideasArr;
           const index = state.ideasArr.findIndex(x => x.id === action.ideaId);
@@ -309,16 +277,10 @@ export function ideas(state = {
     }
     // the api endpoint returns the file uploaded information (metadata - started to upload the file ) 
     case UPLOAD_FILE_SUCCESS: {
-      console.log('reducer:UPLOAD_FILE_SUCCESS');
-      console.log(state);
-      console.log(action);
       if (action.ideaId>=0) { // edit mode
-    	  console.log("editMode");
           const index = state.ideasArr.findIndex(x => x.id === action.ideaId);
           if (index !== -1) {
             let idea = state.ideasArr[index];
-            console.log('idea');
-            console.log(idea);
 
             idea = updateFileOnElement(idea, action.htmlFormFile, action.uploadedFileMeta) 
             
@@ -327,15 +289,12 @@ export function ideas(state = {
               idea,
               ...state.ideasArr.slice(index + 1),
             ];
-            console.log('newIdeas');
-            console.log(newIdeas);
             return Object.assign({}, state, {
               ideasArr: newIdeas,
             });
           }
     	  
       } else {  // add mode  - action.ideaId == -1
-    	  console.log("addMode");
     	  let idea = state.ideaToAdd;
           idea = updateFileOnElement(idea, action.htmlFormFile, action.uploadedFileMeta) 
           
@@ -348,10 +307,6 @@ export function ideas(state = {
 
     
     case UPLOAD_FILE_ON_NEW_COMMENT_REQUEST: {
-        console.log('UPLOAD_FILE_ON_NEW_COMMENT_REQUEST');
-        console.log(action.file);
-        console.log(state);
-        console.log(action);
         
         const index = state.commentsToAdd.findIndex(x => x.ideaId === action.ideaId);
         let comment = {};
@@ -383,14 +338,7 @@ export function ideas(state = {
       }
     // the api endpoint returns the file uploaded information (metadata - started to upload the file ) 
     case UPLOAD_FILE_ON_NEW_COMMENT_SUCCESS: {
-      console.log('reducer:UPLOAD_FILE_ON_NEW_COMMENT_SUCCESS');
-      console.log(state);
-      console.log(action);
-
-      
       const index = state.commentsToAdd.findIndex(x => x.ideaId === action.ideaId);
-      console.log("index");
-      console.log(index);
       let comment = {};
       if (index == -1) {
     	comment = {ideaId: action.ideaId, files: [action.htmlFormFile]}
@@ -404,19 +352,13 @@ export function ideas(state = {
          });
       } else  {
         comment = state.commentsToAdd[index];
-        console.log("comment");
-        console.log(comment);
         comment = updateFileOnElement(comment, action.htmlFormFile, action.uploadedFileMeta) 
-        console.log("comment");
-        console.log(comment);
             
         const newCommentsToAdd = [
            ...state.commentsToAdd.slice(0, index),
            comment,
            ...state.commentsToAdd.slice(index + 1),
         ];
-        console.log("newCommentsToAdd");
-        console.log(newCommentsToAdd);
         return Object.assign({}, state, {
            commentsToAdd: newCommentsToAdd,
         });
@@ -427,13 +369,10 @@ export function ideas(state = {
     
     // finished to upload file (uploaded completely)
     case UPLOAD_FILE_CONTENT_SUCCESS: {
-      console.log('reducer:UPLOAD_FILE_CONTENT_SUCCESS');
       if (action.ideaId >= 0 ) { // edit idea
           const index = state.ideasArr.findIndex(x => x.id === action.ideaId);
           if (index !== -1) {
             let idea = state.ideasArr[index];
-            console.log('idea');
-            console.log(idea);
 
             idea = completeFileOnElement(idea, action.htmlFormFile, action.uploadedFileMeta);
             const newIdeas = [
@@ -441,8 +380,6 @@ export function ideas(state = {
               idea,
               ...state.ideasArr.slice(index + 1),
             ];
-            console.log('newIdeas');
-            console.log(newIdeas);
             return Object.assign({}, state, {
               ideasArr: newIdeas,
             });
